11a215b5e2e0a22ddcdfe510bad6c2f716fb314b
Niels Wouda <nielswouda@gmail.com>
2022-07-06 21:21:47 +0200

    Finish CMake-based build

--- hgs_vrptw/include/Matrix.h
+++ hgs_vrptw/include/Matrix.h
@@ -5,21 +5,18 @@
 #include <vector>
 
 // Implementation of a matrix in a C++ vector
-// This class is used because a flat vector is faster than a vector of vectors which requires two
-// lookup operations rather than one to index a matrix element
+// This class is used because a flat vector is faster than a vector of vectors which requires two lookup operations rather than one to index a matrix element
 class Matrix
 {
-    int cols_;               // The number of columns of the matrix
-    std::vector<int> data_;  // The vector where all the data is stored (this represents the matrix)
+    int cols_;                  // The number of columns of the matrix
+    std::vector<int> data_;     // The vector where all the data is stored (this represents the matrix)
 
 public:
     // Empty constructor: with zero columns and a vector of size zero
     Matrix() : cols_(0), data_(std::vector<int>(0))
-    {
-    }
+    {}
 
-    // Constructor: create a matrix of size dimension by dimension, using a C++ vector of size
-    // dimension * dimension
+    // Constructor: create a matrix of size dimension by dimension, using a C++ vector of size dimension * dimension 
     Matrix(const int dimension) : cols_(dimension)
     {
         data_ = std::vector<int>(dimension * dimension);
