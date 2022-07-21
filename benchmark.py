5a0706f8ce9a5cf0d3e2c040d44b320b16a3face
Niels Wouda <N-Wouda@users.noreply.github.com>
2022-07-21 11:25:47 +0200

    Add benchmark script (#10)
    * Add benchmark script
    
    * Print error message when solution is wrong, track number of issues
    
    * Align values right, print objectives as integers
    
    * Add progress bar to benchmark script
    
    * Move instance/var generation into the tools file, and simplify benchmark a little

---
+++ benchmark.py
@@ -0,0 +1,93 @@
+import argparse
+from datetime import datetime, timedelta
+from functools import partial
+from glob import glob
+from pathlib import Path
+
+import numpy as np
+from tqdm.contrib.concurrent import process_map
+
+import tools
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--seed", type=int, default=1)
+    parser.add_argument("--time_limit", type=int, default=10)
+    parser.add_argument("--num_procs", type=int, default=4)
+    parser.add_argument("--instance_pattern",
+                        default="instances/ORTEC-VRPTW-ASYM-*.txt")
+
+    return parser.parse_args()
+
+
+def solve(loc: str, seed: int, time_limit: int):
+    path = Path(loc)
+
+    hgspy = tools.get_hgspy_module()
+    instance = tools.read_vrplib(path)
+    start = datetime.now()
+
+    config = hgspy.Config(seed=seed, nbVeh=-1)
+    params = hgspy.Params(config, **tools.inst_to_vars(instance))
+
+    rng = hgspy.XorShift128(seed=seed)
+    ls = hgspy.LocalSearch(params, rng)
+    pop = hgspy.Population(params, rng, ls)
+
+    algo = hgspy.GeneticAlgorithm(params, rng, pop)
+    res = algo.run_until(start + timedelta(seconds=time_limit))
+
+    best = res.get_best_found()
+    routes = [route for route in best.get_routes() if route]
+    cost = best.cost()
+
+    try:
+        actual_cost = tools.validate_static_solution(instance, routes)
+        has_issue = False
+
+        assert np.isclose(actual_cost, cost), "Could not validate objective."
+    except AssertionError:
+        has_issue = True
+
+    return path.stem, int(cost), res.get_num_iters(), has_issue
+
+
+def tabulate(headers, rows) -> str:
+    # These lengths are used to space each column properly.
+    lengths = [len(header) for header in headers]
+
+    for row in rows:
+        for idx, cell in enumerate(row):
+            lengths[idx] = max(lengths[idx], len(str(cell)))
+
+    lines = ["  ".join(f"{h:<{l}s}" for l, h in zip(lengths, headers)),
+             "  ".join("-" * l for l in lengths)]
+
+    for row in rows:
+        lines.append("  ".join(f"{str(c):>{l}s}" for l, c in zip(lengths, row)))
+
+    return "\n".join(lines)
+
+
+def main():
+    args = parse_args()
+
+    func = partial(solve, seed=args.seed, time_limit=args.time_limit)
+    func_args = glob(args.instance_pattern)
+    tqdm_kwargs = dict(max_workers=args.num_procs, unit="instance")
+    data = process_map(func, func_args, **tqdm_kwargs)
+
+    dtype = [('inst', 'U37'), ('obj', int), ('iters', int), ('issues', bool)]
+    data = np.array(data, dtype=dtype)
+    table = tabulate(["Inst.", "Obj.", "Iters.", "Issue?"], data)
+
+    print('\n', table, '\n', sep="")
+    print(f" Avg. objective: {data['obj'].mean():.0f}")
+    print(f"Avg. iterations: {data['iters'].mean():.0f}")
+    print(f"   Total issues: {data['issues'].sum()}")
+
+
+if __name__ == "__main__":
+    main()
--- tools.py
+++ tools.py
@@ -1,5 +1,9 @@
+import glob
+import importlib.machinery
+import importlib.util
 import json
 import os
+
 import numpy as np
 
 # https://stackoverflow.com/questions/26646362/numpy-array-is-not-json-serializable
@@ -317,3 +321,28 @@ def write_vrplib(filename, instance, name="problem", euclidean=False, is_vrptw=T
                 f.write("\n")
             
         f.write("EOF\n")
+
+
+def get_hgspy_module(where: str = 'release/lib/hgspy*.so'):
+    lib_path = next(glob.iglob(where))
+    loader = importlib.machinery.ExtensionFileLoader('hgspy', lib_path)
+    spec = importlib.util.spec_from_loader(loader.name, loader)
+    hgspy = importlib.util.module_from_spec(spec)
+    loader.exec_module(hgspy)
+
+    return hgspy
+
+
+def inst_to_vars(inst):
+    # Notice that the dictionary key names are not entirely free-form: these
+    # should match the argument names defined in the C++/Python bindings.
+
+    # TODO obsolete this by allowing numpy arguments to the bindings.
+    return dict(
+        coords=[(x, y) for x, y in inst['coords'].tolist()],
+        demands=inst['demands'].tolist(),
+        vehicle_cap=inst['capacity'],
+        time_windows=[(l, u) for l, u in inst['time_windows'].tolist()],
+        service_durations=inst['service_times'].tolist(),
+        duration_matrix=inst['duration_matrix'].tolist(),
+    )
