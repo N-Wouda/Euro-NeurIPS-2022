c8b4312c024402dab113ad9224c2122fc3d60295
Leon Lan <leon.lanyidong@gmail.com>
2022-08-01 22:11:05 +0200

    Run clang-format again

--- hgs_vrptw/src/Statistics.cpp
+++ hgs_vrptw/src/Statistics.cpp
@@ -13,10 +13,10 @@ void Statistics::collectFrom(Population const &population)
     iterTimes.push_back(diff.count());
     lastIter = clock::now();  // update for next call
 
-    auto const numFeas = std::count_if(population.population.begin(),
-                                       population.population.end(),
-                                       [](Individual const *indiv)
-                                       { return indiv->isFeasible(); });
+    auto const numFeas = std::count_if(
+        population.population.begin(),
+        population.population.end(),
+        [](Individual const *indiv) { return indiv->isFeasible(); });
 
     numFeasible.push_back(numFeas);
 
@@ -27,8 +27,9 @@ void Statistics::collectFrom(Population const &population)
         population.population.begin(),
         population.population.end(),
         0.,
-        [](double val, Individual const *indiv)
-        { return val + indiv->avgBrokenPairsDistanceClosest(); });
+        [](double val, Individual const *indiv) {
+            return val + indiv->avgBrokenPairsDistanceClosest();
+        });
 
     popDiversity_.push_back(totalDiversity / static_cast<double>(nPops));
 
