663f96d222950fa9ffbe6e82eb073d3737cd0370
Leon Lan <leon.lanyidong@gmail.com>
2022-08-11 14:12:37 +0200

    Add numIters and runTime methods to Result

--- hgs_vrptw/include/Result.h
+++ hgs_vrptw/include/Result.h
@@ -4,17 +4,27 @@
 #include "Individual.h"
 #include "Statistics.h"
 
+#include <chrono>
 #include <utility>
 #include <vector>
 
 class Result
 {
+    using duration = std::chrono::duration<double>;
     Individual const &bestFound;
     Statistics stats;
+    size_t numIters;
+    duration runTime;
 
 public:
-    Result(Individual const &bestFound, Statistics stats)
-        : bestFound(bestFound), stats(std::move(stats))
+    Result(Individual const &bestFound,
+           Statistics stats,
+           size_t numIters,
+           duration runTime)
+        : bestFound(bestFound),
+          stats(std::move(stats)),
+          numIters(numIters),
+          runTime(runTime)
     {
     }
 
@@ -27,6 +37,16 @@ public:
      * Returns statistics collected by the genetic algorithm.
      */
     [[nodiscard]] Statistics const &getStatistics() const { return stats; }
+
+    /**
+     * Returns the total number of iterations.
+     */
+    [[nodiscard]] size_t const &getIterations() const { return numIters; }
+
+    /**
+     * Returns the total elapsed runtime in seconds.
+     */
+    [[nodiscard]] duration const &getRunTime() const { return runTime; }
 };
 
 #endif  // RESULT_H
