5372965cbdafe09dae20cf429e6a579049ff18ee
Leon Lan <leon.lanyidong@gmail.com>
2022-08-19 11:33:50 +0200

    Add step size argument in plotting

--- plotting.py
+++ plotting.py
@@ -1,16 +1,22 @@
-def x_axis(stats, plot_runtimes):
+import numpy as np
+
+
+def x_axis(stats, step, plot_runtimes):
     if plot_runtimes:
-        return stats.run_times(), "Run-time (s)"
+        return stats.run_times()[::step], "Run-time (s)"
     else:
-        return stats.curr_iters(), "Iteration (#)"
+        return np.arange(0, stats.num_iters(), step), "Iteration (#)"
 
 
-def plot_population(stats, ax, plot_runtimes=False):
-    x_vals, x_label = x_axis(stats, plot_runtimes)
+def plot_population(stats, ax, step=1, plot_runtimes=False):
+    x_vals, x_label = x_axis(stats, step, plot_runtimes)
 
     # Number feasible individuals
     ax.plot(
-        x_vals, stats.num_feasible_pop(), label="# Feasible", c="tab:orange"
+        x_vals,
+        stats.num_feasible_pop()[::step],
+        label="# Feasible",
+        c="tab:orange",
     )
 
     ax.set_title("Population statistics")
@@ -20,17 +26,31 @@ def plot_population(stats, ax, plot_runtimes=False):
 
     # Population diversity
     ax_div = ax.twinx()
-    ax_div.plot(x_vals, stats.pop_diversity(), label="Diversity", c="tab:red")
+    ax_div.plot(
+        x_vals, stats.pop_diversity()[::step], label="Diversity", c="tab:red"
+    )
     ax_div.set_ylabel("Avg. diversity")
     ax_div.legend(frameon=False)
 
 
-def plot_objectives(stats, ax, plot_runtimes=False):
-    x_vals, x_label = x_axis(stats, plot_runtimes)
+def plot_objectives(stats, ax, step=1, plot_runtimes=False):
+    x_vals, x_label = x_axis(stats, step, plot_runtimes)
 
-    ax.plot(x_vals, stats.best_objectives(), label="Best", c="tab:blue")
-    ax.plot(x_vals, stats.feas_objectives(), label="Feasible", c="tab:green")
-    ax.plot(x_vals, stats.infeas_objectives(), label="Infeasible", c="tab:red")
+    ax.plot(
+        x_vals, stats.best_objectives()[::step], label="Best", c="tab:blue"
+    )
+    ax.plot(
+        x_vals,
+        stats.feas_objectives()[::step],
+        label="Feasible",
+        c="tab:green",
+    )
+    ax.plot(
+        x_vals,
+        stats.infeas_objectives()[::step],
+        label="Infeasible",
+        c="tab:red",
+    )
 
     ax.set_title("Population objectives")
     ax.set_xlabel(x_label)
