2c495c4438b6ae24c38bdbb39a5eff6f8bf6d2d9
Leon <30997278+leonlan@users.noreply.github.com>
2022-09-10 16:18:34 +0200

    Remove giant tour representation and crossovers (#80)
    * Remove AEX and OX
    
    * Remove Individual tour constructor
    
    * Remove makeRoutes; refactor random initial solution
    
    * Remove tour_ and Split-stuff
    
    * Add comments to random indiv constructor
    
    * Rewriting/ordering
    
    * Fix typo
    
    * Refactor random initial solution
    
    * Update comment, remove unused headers

--- hgs_vrptw/include/crossover.h
+++ hgs_vrptw/include/crossover.h
@@ -18,16 +18,6 @@ void greedyRepair(std::vector<std::vector<int>> &routes,
 }  // namespace crossover
 
 /**
- * Does two ordered crossovers of the given parents (binary tournament). Each
- * crossover randomly selects a subset of clients from the first parent, and the
- * rest comes from the second parent.
- */
-Individual orderedExchange(
-    std::pair<Individual const *, Individual const *> const &parents,
-    Params const &params,
-    XorShift128 &rng);
-
-/**
  * Performs two SREX crossovers of the given parents (binary tournament). This
  * was one of ORTEC's DIMACS contributions.
  * <br />
@@ -37,21 +27,6 @@ Individual orderedExchange(
  * Nature. Springer. 2010, pp. 536–545.
  */
 Individual selectiveRouteExchange(
-    std::pair<Individual const *, Individual const *> const &parents,
-    Params const &params,
-    XorShift128 &rng);
-
-/**
- * Creates an offspring by selecting alternately the next client of the first
- * parent and the next client of the second parent, omitting clients already
- * present in the offspring.
- * <br />
- * P. Larrañaga , C. Kuijpers, R. Murga, Y Yurramendi. 1996. "Learning Bayesian
- * network structures by searching for the best ordering with genetic
- * algorithms". IEEE Transactions on Systems, Man and Cybernetics. 26, pp.
- * 487-493.
- */
-Individual alternatingExchange(
     std::pair<Individual const *, Individual const *> const &parents,
     Params const &params,
     XorShift128 &rng);
--- hgs_vrptw/src/Individual.cpp
+++ hgs_vrptw/src/Individual.cpp
@@ -1,154 +1,9 @@
 #include "Individual.h"
 #include "Params.h"
 
-#include <cassert>
-#include <deque>
 #include <fstream>
 #include <numeric>
 #include <vector>
-
-namespace
-{
-// Structure representing a client when making routes
-struct ClientSplit
-{
-    int demand = 0;       // The demand of the client
-    int serviceTime = 0;  // The service duration of the client
-    int d0_x = 0;         // The distance from the depot to the client
-    int dx_0 = 0;         // The distance from the client to the depot
-    int dnext = 0;        // The distance from the client to the next client
-};
-
-struct ClientSplits
-{
-    Params const &params;
-
-    std::vector<ClientSplit> splits;
-    std::vector<int> predecessors;
-    std::vector<int> pathCosts;
-
-    std::vector<int> cumDist;
-    std::vector<int> cumLoad;
-    std::vector<int> cumServ;
-
-    ClientSplits(Params const &params, std::vector<int> const &tour)
-        : params(params),
-          splits(params.nbClients + 1),
-          predecessors(params.nbClients + 1, 0),
-          pathCosts(params.nbClients + 1, INT_MAX),
-          cumDist(params.nbClients + 1, 0),
-          cumLoad(params.nbClients + 1, 0),
-          cumServ(params.nbClients + 1, 0)
-    {
-        pathCosts[0] = 0;
-
-        for (int idx = 1; idx <= params.nbClients; idx++)  // exclude depot
-        {
-            auto const curr = tour[idx - 1];
-            auto const dist = idx < params.nbClients  // INT_MIN for last client
-                                  ? params.dist(curr, tour[idx])
-                                  : INT_MIN;
-
-            splits[idx] = {params.clients[curr].demand,
-                           params.clients[curr].servDur,
-                           params.dist(0, curr),
-                           params.dist(curr, 0),
-                           dist};
-
-            cumLoad[idx] = cumLoad[idx - 1] + splits[idx].demand;
-            cumServ[idx] = cumServ[idx - 1] + splits[idx].serviceTime;
-            cumDist[idx] = cumDist[idx - 1] + splits[idx - 1].dnext;
-        }
-    }
-
-    // Computes the cost of propagating label i to j
-    [[nodiscard]] int propagate(int i, int j) const
-    {
-        assert(i < j);
-        auto const deltaDist = cumDist[j] - cumDist[i + 1];
-        return pathCosts[i] + deltaDist + splits[i + 1].d0_x + splits[j].dx_0
-               + params.loadPenalty(cumLoad[j] - cumLoad[i]);
-    }
-
-    // Tests if i dominates j as a predecessor for all nodes x >= j + 1
-    [[nodiscard]] bool leftDominates(int i, int j) const
-    {
-        assert(i < j);
-        auto const lhs = pathCosts[j] + splits[j + 1].d0_x;
-        auto const deltaDist = cumDist[j] - cumDist[i + 1];
-        auto const rhs = pathCosts[i] + splits[i + 1].d0_x + deltaDist
-                         + params.penaltyCapacity * (cumLoad[j] - cumLoad[i]);
-
-        return lhs >= rhs;
-    }
-
-    // Tests if j dominates i as a predecessor for all nodes x >= j + 1
-    [[nodiscard]] bool rightDominates(int i, int j) const
-    {
-        assert(i < j);
-        auto const lhs = pathCosts[j] + splits[j + 1].d0_x;
-        auto const rhs = pathCosts[i] + splits[i + 1].d0_x + cumDist[j + 1]
-                         - cumDist[i + 1];
-
-        return lhs <= rhs;
-    };
-};
-}  // namespace
-
-void Individual::makeRoutes()
-{
-    ClientSplits splits(*params, getTour());
-
-    auto deq = std::deque<int>(params->nbClients + 1);
-    deq.push_front(0);  // depot
-
-    for (int idx = 1; idx <= params->nbClients; idx++)  // exclude depot
-    {
-        splits.pathCosts[idx] = splits.propagate(deq.front(), idx);
-        splits.predecessors[idx] = deq.front();  // best predecessor for idx
-
-        if (idx == params->nbClients)
-            break;
-
-        // idx will be inserted if idx is not dominated by the last client: we
-        // need to remove whoever is dominated by idx.
-        if (!splits.leftDominates(deq.back(), idx))
-        {
-            while (!deq.empty() && splits.rightDominates(deq.back(), idx))
-                deq.pop_back();
-
-            deq.push_back(idx);
-        }
-
-        while (deq.size() >= 2)  // Check if the current front is dominated by
-        {                        // the follow-up client. If so, remove.
-            auto const firstProp = splits.propagate(deq[0], idx + 1);
-            auto const secondProp = splits.propagate(deq[1], idx + 1);
-
-            if (firstProp >= secondProp)
-                deq.pop_front();
-            else
-                break;
-        }
-    }
-
-    if (splits.pathCosts[params->nbClients] == INT_MAX)  // has not been updated
-        throw std::runtime_error("No split solution reached the last client");
-
-    int end = params->nbClients;
-    for (auto &route : routes_)
-    {
-        if (end == 0)
-            break;
-
-        int begin = splits.predecessors[end];
-        route = {tour_.begin() + begin, tour_.begin() + end};
-        end = begin;
-    }
-
-    assert(end == 0);
-    evaluateCompleteCost();
-}
 
 void Individual::evaluateCompleteCost()
 {
@@ -160,8 +15,8 @@ void Individual::evaluateCompleteCost()
 
     for (auto const &route : routes_)
     {
-        if (route.empty())  // First empty route. Due to makeRoutes() all
-            break;          // subsequent routes are empty as well
+        if (route.empty())  // First empty route. All subsequent routes are
+            break;          // empty as well
 
         nbRoutes++;
 
@@ -294,47 +149,39 @@ void Individual::makeNeighbours()
 
 Individual::Individual(Params const *params, XorShift128 *rng)
     : params(params),
-      tour_(params->nbClients),
       routes_(params->nbVehicles),
       neighbours(params->nbClients + 1)
 {
-    std::iota(tour_.begin(), tour_.end(), 1);
-    std::shuffle(tour_.begin(), tour_.end(), *rng);
+    // Sort clients randomly
+    auto clients = std::vector<int>(params->nbClients);
+    std::iota(clients.begin(), clients.end(), 1);
+    std::shuffle(clients.begin(), clients.end(), *rng);
 
-    makeRoutes();
-    makeNeighbours();
-}
+    // Distribute clients evenly over the routes
+    auto const clientsPerRoute = params->nbClients / params->nbVehicles + 1;
 
-Individual::Individual(Params const *params, Tour tour)
-    : params(params),
-      tour_(std::move(tour)),
-      routes_(params->nbVehicles),
-      neighbours(params->nbClients + 1)
-{
-    makeRoutes();
+    for (auto idx = 0; idx != params->nbClients; ++idx)
+    {
+        auto const client = clients[idx];
+        routes_[idx / clientsPerRoute].push_back(client);
+    }
+
     makeNeighbours();
+    evaluateCompleteCost();
 }
 
 Individual::Individual(Params const *params, Routes routes)
     : params(params),
-      tour_(),
       routes_(std::move(routes)),
       neighbours(params->nbClients + 1)
 {
-    makeNeighbours();
-
     // a precedes b only when a is not empty and b is. Combined with a stable
     // sort, this ensures we keep the original sorting as much as possible, but
     // also make sure all empty routes are at the end of routes_.
     auto comp = [](auto &a, auto &b) { return !a.empty() && b.empty(); };
     std::stable_sort(routes_.begin(), routes_.end(), comp);
 
-    tour_.reserve(params->nbClients);
-
-    for (auto const &route : routes_)
-        for (Client c : route)
-            tour_.push_back(c);
-
+    makeNeighbours();
     evaluateCompleteCost();
 }
 
