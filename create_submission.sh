dcbc958ef76dc9c0bbe5b69d988301a2a3cf7a14
Niels Wouda <nielswouda@gmail.com>
2022-09-15 14:24:18 +0200

    Update scripts

--- create_submission.sh
+++ create_submission.sh
@@ -6,6 +6,14 @@ mkdir -p tmp/submissions
 # C++ source and header files (incl. pybind)
 find hgs_vrptw \( -name "*.cpp" -o -name "*.h" -o -name "CMakeLists.txt" -o -name "*.cmake" \) -exec zip -o -r "tmp/submissions/$DATE.zip" {} +
 
+# Required CodaLab file
+cat > metadata <<- META
+description: This file is required to indicate a code submission on the CodaLab platform. There is no need to change this file.
+META
+
+zip -o -r "tmp/submissions/$DATE.zip" metadata
+rm metadata
+
 # Python dependencies need to be provided as a requirements.txt, since we can
 # only use pip in the competition system.
 cat > requirements.txt <<- REQ
@@ -18,6 +26,6 @@ rm requirements.txt
 
 # Python code and install/run scripts
 zip -o -r "tmp/submissions/$DATE.zip" solver.py tools.py strategies.py environment.py
-zip -o -r "tmp/submissions/$DATE.zip" metadata run.sh install.sh
+zip -o -r "tmp/submissions/$DATE.zip" run.sh install.sh
 
 echo "Created submissions/submission_$DATE.zip"
--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 # C++ code
 mkdir -p release
 cmake -Brelease -Shgs_vrptw -DCMAKE_BUILD_TYPE=Release
-cd release && make && cd ..
+make  --directory=release
