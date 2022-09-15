ce373c026ef0ef53c96a33ec7b9ff0a4e83d57b3
Niels Wouda <nielswouda@gmail.com>
2022-09-15 14:42:56 +0200

    Remove other scripts, do not pack pybind tests/build files into submission

--- create_submission.sh
+++ create_submission.sh
@@ -3,8 +3,14 @@
 DATE=$(date "+%Y-%m-%d-%H-%M-%S")
 mkdir -p tmp/submissions
 
-# C++ source and header files (incl. pybind)
-find hgs_vrptw \( -name "*.cpp" -o -name "*.h" -o -name "CMakeLists.txt" -o -name "*.cmake" \) -exec zip -o -r "tmp/submissions/$DATE.zip" {} +
+# C++ source and header files (incl. pybind11)
+find hgs_vrptw \
+  -wholename "*/pybind11/tests/*" -prune -o -wholename "*/pybind11/build/*" -prune \
+  -o \( -name "*.cpp" -o -name "*.h" -o -name "CMakeLists.txt" -o -name "*.cmake" \) \
+  -exec zip -o -r "tmp/submissions/$DATE.zip" {} +
+
+# Python code
+zip -o -r "tmp/submissions/$DATE.zip" solver.py tools.py strategies.py environment.py
 
 # Required CodaLab file
 cat > metadata <<- META
@@ -24,8 +30,27 @@ REQ
 zip -o -r "tmp/submissions/$DATE.zip" requirements.txt
 rm requirements.txt
 
-# Python code and install/run scripts
-zip -o -r "tmp/submissions/$DATE.zip" solver.py tools.py strategies.py environment.py
-zip -o -r "tmp/submissions/$DATE.zip" run.sh install.sh
+# Run and install scripts needed for CodaLab
+cat > install.sh <<- INSTALL
+#!/bin/bash
 
-echo "Created submissions/submission_$DATE.zip"
+# Python dependencies
+pip install -r requirements.txt
+
+# C++ code
+mkdir -p release
+cmake -Brelease -Shgs_vrptw -DCMAKE_BUILD_TYPE=Release
+make --directory=release
+INSTALL
+
+cat > run.sh <<- RUN
+#!/bin/bash
+
+python solver.py
+RUN
+
+zip -o -r "tmp/submissions/$DATE.zip" run.sh install.sh
+rm install.sh
+rm run.sh
+
+echo "Created tmp/submissions/submission_$DATE.zip"
--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Python dependencies
-pip install -r requirements.txt
-
-# C++ code
-mkdir -p release
-cmake -Brelease -Shgs_vrptw -DCMAKE_BUILD_TYPE=Release
-make --directory=release
