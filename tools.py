Fix request_idx for delta cost computation

tools.py
@@ -432,9 +432,10 @@ def n_vehicles_bin_pack(instance, margin=1.5):
     return math.ceil(margin * total_demand / instance['capacity']) + 3
 
 
-def get_node_costs(routes, duration_matrix):
+def get_node_costs(routes, duration_matrix, request_idx=None):
     node_costs = {}
     for route in routes:
-        node_costs.update(zip(route, duration_matrix[np.append(0, route)] + duration_matrix[np.append(route, 0)]))
+        node_costs.update(zip(route if request_idx is None else request_idx[route],
+                              duration_matrix[np.append(0, route)] + duration_matrix[np.append(route, 0)]))
 
     return node_costs