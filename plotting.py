3d6e221bcec5f8238b09dc765676ca5577dff81d
Leon Lan <leon.lanyidong@gmail.com>
2022-09-23 12:21:50 +0200

    Run black

--- plotting.py
+++ plotting.py
@@ -20,11 +20,11 @@ def plot_instance(ax, instance, routes=()):
 
     A given list of routes can also be plotted, if provided.
     """
-    is_client = ~instance['is_depot']
-    coords = instance['coords'][is_client].T
+    is_client = ~instance["is_depot"]
+    coords = instance["coords"][is_client].T
     tws_open = instance["time_windows"][is_client, 0]
     tws_close = instance["time_windows"][is_client, 1]
-    depot_coords = instance['coords'][~is_client].T
+    depot_coords = instance["coords"][~is_client].T
 
     kwargs = dict(s=(0.0003 * tws_open) ** 2, zorder=3)
     ax.scatter(*coords, c="tab:blue", label="TW open", **kwargs)
@@ -36,12 +36,12 @@ def plot_instance(ax, instance, routes=()):
     ax.scatter(*depot_coords, c="tab:red", label="Depot", **kwargs)
 
     for route in routes:
-        ax.plot(*instance['coords'][[0] + route + [0]].T)
+        ax.plot(*instance["coords"][[0] + route + [0]].T)
 
-    ax.grid(color='grey', linestyle='--', linewidth=0.25)
+    ax.grid(color="grey", linestyle="--", linewidth=0.25)
 
     ax.set_title("Solution" if routes else "Instance")
-    ax.set_aspect('equal', 'datalim')
+    ax.set_aspect("equal", "datalim")
     ax.legend(frameon=False, ncol=3)
 
 
