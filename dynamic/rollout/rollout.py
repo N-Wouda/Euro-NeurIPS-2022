41ab975d6d0080c447aba87567fc95c1f16468e2
Leon Lan <leon.lanyidong@gmail.com>
2022-09-26 10:10:15 +0200

    Refactor rollout

--- dynamic/rollout/rollout.py
+++ dynamic/rollout/rollout.py
@@ -25,31 +25,26 @@ def rollout(info, obs, rng):
     if obs["current_epoch"] in [info["start_epoch"], info["end_epoch"]]:
         return obs["epoch_instance"]
 
-    static_inst = info["dynamic_context"]
-    ep_inst = obs["epoch_instance"]
-    start_time = obs["planning_starttime"]
-
-    sim_tlim = info["epoch_tlim"] * SIM_TLIM_FACTOR
     start = time.perf_counter()
 
-    # REVIEW maybe we need to include something about the route cost
-    # alongside the delta cost. the delta cost is probably not a good
-    # enough indicator of how expensive a client is. We also need
-    # track something about how effective a route is used.
-    n_ep_requests = len(ep_inst["coords"])  # include depot for idx
-    dispatch_actions = np.zeros(n_ep_requests, dtype=int)
-    dispatch_costs = np.zeros(n_ep_requests, dtype=int)
-    postpone_costs = np.zeros(n_ep_requests, dtype=int)
+    # Parameters
+    static_inst = info["dynamic_context"]
+    ep_inst = obs["epoch_instance"]
+    planning_start_time = obs["planning_starttime"]
+    n_requests = len(ep_inst["coords"])
+    sim_tlim = info["epoch_tlim"] * SIM_TLIM_FACTOR
 
+    # Statistics
     n_simulations = 0
+    dispatch_actions = np.zeros(n_requests, dtype=int)
 
     while time.perf_counter() - start < sim_tlim:
         sim_inst = simulate_instance(
-            static_inst, ep_inst, N_LOOKAHEAD, start_time, rng
+            static_inst, ep_inst, N_LOOKAHEAD, planning_start_time, rng
         )
 
-        try:  # Simulations may be infeasible within TLIM
-            # Sim_sol is has indices 1, ..., N
+        try:  # TODO create new solver that doesnt raise infeasibility error
+            # sim_sol is has indices 1, ..., N
             sim_sol, _ = solve_static(
                 sim_inst, max_iterations=SIM_SOLVE_ITERS, **sim_config
             )
@@ -58,41 +53,25 @@ def rollout(info, obs, rng):
             req_sol = utils.sol2ep(sim_sol, sim_inst, postpone_routes=False)
 
             for sim_route, req_route in zip(sim_sol, req_sol):
-                # Routes with req. idcs >= START_IDX are postponed
-                action = 0 if (req_route >= START_IDX).any() else 1
-
-                delta_costs = utils.delta_cost(
-                    sim_route, sim_inst["duration_matrix"]
-                )
-
-                for idx, request, delta in zip(
-                    sim_route, req_route, delta_costs
-                ):
-                    if request >= START_IDX:
-                        continue
-
-                    dispatch_actions[idx] += action
-
-                    if action:
-                        dispatch_costs[idx] += delta
-                    else:
-                        postpone_costs[idx] += delta
+                # Routes with req. idcs < START_IDX are dispatched now
+                # TODO could be made faster if we check any & use postpone_actions
+                if all(req_route < START_IDX):
+                    dispatch_actions[sim_route] += 1
 
             n_simulations += 1
         except AssertionError as e:
             pass
 
+    print(n_simulations)
+
+    # Postpone clients that are often postponed and non-urgent
     dispatch_fraction = dispatch_actions / np.maximum(1, n_simulations)
-    mean_dispatch_costs = dispatch_costs * dispatch_fraction
-    mean_postpone_costs = postpone_costs * (1 - dispatch_fraction)
+    postpone = dispatch_fraction <= 1 - POSTPONE_THRESHOLD
+    non_urgent = ~ep_inst["must_dispatch"]
 
-    mask = np.full(n_ep_requests, True)
-
-    # Postpone clients that are often postponed and not must-dispatch
-    postpone = (dispatch_fraction <= 1 - POSTPONE_THRESHOLD) & ~ep_inst[
-        "must_dispatch"
-    ]
-    mask = np.where(postpone, False, mask)
+    # Mask contains True for dispatch, False for postpone
+    mask = np.full(n_requests, True)
+    mask = np.where(postpone & non_urgent, False, mask)
     mask[0] = True
 
     return utils.filter_instance(ep_inst, mask)
