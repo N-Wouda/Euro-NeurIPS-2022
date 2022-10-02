4bd197fa58480d928ea217eee7859e2159bbfdce
Jasper van Doorn <jasper.vandoorn@hotmail.com>
2022-10-02 02:09:42 +0200

    Move run_oracle into dynamic.solver

--- dynamic/__init__.py
+++ dynamic/__init__.py
@@ -1,5 +1,4 @@
 from . import strategies
 from . import utils
 
-from .solver import solve_dynamic, solve_epoch
-from .run_oracle import run_oracle
+from .solver import solve_dynamic, solve_epoch, run_oracle
--- dynamic/solver.py
+++ dynamic/solver.py
@@ -64,3 +64,44 @@ def solve_dynamic(env, dispatch_strategy, **kwargs):
         assert info["error"] is None, info["error"]
 
     return env.final_costs, env.final_solutions
+
+
+def run_oracle(env, **kwargs):
+    """
+    Solve the dynamic VRPTW problem using the oracle strategy, i.e., the
+    problem is solved as static VRPTW with release dates using the information
+    that is known in hindsight. The found solution is then submitted to the
+    environment.
+    """
+    observation, info = env.reset()
+    ep_tlim = info["epoch_tlim"]
+    done = False
+
+    # Submit dummy solutions to obtain the hindsight problem
+    while not done:
+        requests = observation["epoch_instance"]["request_idx"][1:]
+        ep_sol = [[request] for request in requests]
+        observation, _, done, _ = env.step(ep_sol)
+
+    hindsight_inst = env.get_hindsight_problem()
+    solution, _ = solve_epoch(hindsight_inst, time_limit=ep_tlim)
+
+    observation, _ = env.reset()
+
+    # Submit the solution from the hindsight problem
+    while not env.is_done:
+        ep_inst = observation["epoch_instance"]
+        requests = set(ep_inst["request_idx"])
+
+        # This is a proxy to extract the routes from the hindsight
+        # solution that are dispatched in the current epoch.
+        ep_sol = [
+            route
+            for route in solution
+            if len(requests.intersection(route)) == len(route)
+        ]
+
+        observation, reward, done, info = env.step(ep_sol)
+        assert info["error"] is None, f"{info['error']}"
+
+    return env.final_costs, env.final_solutions
