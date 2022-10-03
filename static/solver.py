3e30e9b89c29a87eec63498af96b1b986615a04c
Jasper van Doorn <jasper.vandoorn@hotmail.com>
2022-10-03 14:49:32 +0200

    Run black and flake8

--- static/solver.py
+++ static/solver.py
@@ -5,7 +5,15 @@ import tools
 hgspy = tools.get_hgspy_module()
 
 
-def solve(instance, *, seed=1, max_runtime=None, max_iterations=None, initial_solutions=(), mode="static"):
+def solve(
+    instance,
+    *,
+    seed=1,
+    max_runtime=None,
+    max_iterations=None,
+    initial_solutions=(),
+    mode="static",
+):
     configuration = toml.load(f"configurations/{mode}.toml")
 
     config = hgspy.Config(seed=seed, **configuration["params"])
@@ -14,26 +22,37 @@ def solve(instance, *, seed=1, max_runtime=None, max_iterations=None, initial_so
     rng = hgspy.XorShift128(seed=seed)
     pop = hgspy.Population(params, rng)
 
-    initial_individuals = [hgspy.Individual(params, sol) for sol in initial_solutions]
+    initial_individuals = [
+        hgspy.Individual(params, sol) for sol in initial_solutions
+    ]
 
     for indiv in initial_individuals:
         pop.add_individual(indiv)
 
     ls = hgspy.LocalSearch(params, rng)
 
-    node_ops = [getattr(hgspy.operators, op)(params) for op in configuration["operators"]["node"]]
+    node_ops = [
+        getattr(hgspy.operators, op)(params)
+        for op in configuration["operators"]["node"]
+    ]
 
-    for op in node_ops :
+    for op in node_ops:
         ls.add_node_operator(op)
 
-    route_ops = [getattr(hgspy.operators, op)(params) for op in configuration["operators"]["route"]]
+    route_ops = [
+        getattr(hgspy.operators, op)(params)
+        for op in configuration["operators"]["route"]
+    ]
 
     for op in route_ops:
         ls.add_route_operator(op)
 
     algo = hgspy.GeneticAlgorithm(params, rng, pop, ls)
 
-    crossover_ops = [getattr(hgspy.crossover, op) for op in configuration["operators"]["crossover"]]
+    crossover_ops = [
+        getattr(hgspy.crossover, op)
+        for op in configuration["operators"]["crossover"]
+    ]
 
     for op in crossover_ops:
         algo.add_crossover_operator(op)
