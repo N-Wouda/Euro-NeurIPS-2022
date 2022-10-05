588a1b4eafa62d816786446598678888619c3825
Niels Wouda <nielswouda@gmail.com>
2022-10-05 12:38:42 +0200

    Fix probabilities for simple baselines

--- strategies/dynamic/random/__init__.py
+++ strategies/dynamic/random/__init__.py
@@ -2,6 +2,6 @@ from functools import partial as _partial
 
 from .random_dispatch import random_dispatch as _random_dispatch
 
-greedy = _partial(_random_dispatch, prob=100)
+greedy = _partial(_random_dispatch, prob=1)
 lazy = _partial(_random_dispatch, prob=0)
-random = _partial(_random_dispatch, prob=50)
+random = _partial(_random_dispatch, prob=.5)
