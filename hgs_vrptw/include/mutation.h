673504d0c176e6aed3dd5b19ee111e10d2ae1d05
l-lan <l.lan@vu.nl>
2022-10-06 15:50:06 +0200

    Improve dosctring

--- hgs_vrptw/include/mutation.h
+++ hgs_vrptw/include/mutation.h
@@ -8,14 +8,14 @@
 #include <vector>
 
 /**
- * Performs a SISR mutation on the passed-in individual. SISRX first removes
- * strings of clients from based on "Slack Induction String Removals (SISRs)"
- * [1]. The individual is then repaired to a complete solution by greedily
+ * Mutates the passed-in individual by removing strings of clients based on
+ * "Slack Induction String Removals (SISRs)" [1]. After removing a number of
+ * strings, the individual is repaired to a complete solution by greedily
  * re-inserting the unplanned clients.
  * <br />
- * [1]: Christiaens, J., & Vanden Berghe, G. (2020). Slack induction by string
- * removals for vehicle routing problems. Transportation Science, 54(2),
- * 417-433.
+ * [1]: Christiaens, J., & Vanden
+ * Berghe, G. (2020). Slack induction by string removals for vehicle routing
+ * problems. Transportation Science, 54(2), 417-433.
  */
 Individual
 stringRemovals(Individual &indiv, Params const &params, XorShift128 &rng);
