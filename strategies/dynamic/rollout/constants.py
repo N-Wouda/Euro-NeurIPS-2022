1a36ab8df2f9aaa401bf834095a3add1ed6b366c
Leon <30997278+leonlan@users.noreply.github.com>
2022-10-10 15:33:36 +0200

    Improve VRPTW for release times and new rollout config (#132)
    * Add release time to read VRPLIB and change name2size
    
    * Fix type error in release time
    
    * Include release time in Params::calculateNeighbors
    
    * Include release time in greedy repair
    
    * New rollout config
    
    * Update Params::calculateNeighbors and SIM_SOLVE_ITERS
    
    * Format section tools.py
    
    Co-authored-by: Niels Wouda <nielswouda@gmail.com>

--- strategies/dynamic/rollout/constants.py
+++ strategies/dynamic/rollout/constants.py
@@ -2,17 +2,20 @@ EPOCH_N_REQUESTS = 100
 EPOCH_DURATION = 3600
 
 N_LOOKAHEAD = 3  # number of lookahead simulations
-SIM_TLIM_FACTOR = 0.5  # percent of epoch tlim
-SIM_SOLVE_ITERS = 25  # iterations to solve a simulation instance
+SIM_TLIM_FACTOR = 0.6  # percent of epoch tlim
+SIM_SOLVE_ITERS = 40  # iterations to solve a simulation instance
 
-DISPATCH_THRESHOLD = 0.15
+DISPATCH_THRESHOLD = 0.20
 
 # Configuration to solve simulation instances
 SIM_SOLVE_CONFIG = {
     "seed": 1,
-    "generationSize": 20,
-    "minPopSize": 5,
-    "nbGranular": 80,
-    "initialTimeWarpPenalty": 250,
-    "penaltyDecrease": 0.5,
+    "generationSize": 2,
+    "minPopSize": 1,
+    "nbGranular": 70,
+    "initialTimeWarpPenalty": 25,
+    "penaltyDecrease": 0.25,
+    "nbPenaltyManagement": 1,
+    "repairProbability": 0,
+    "intensificationProbability": 0,
 }
