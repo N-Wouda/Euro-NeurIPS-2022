1092551302a0f1f465256d695a3875ee9ca0b6c3
Niels Wouda <nielswouda@gmail.com>
2022-10-10 22:16:44 +0200

    Run black and flake8

--- hgspy.py
+++ hgspy.py
@@ -4,9 +4,9 @@ import importlib.util
 import sys
 
 
-def get_module(where: str = 'release/lib/hgspy*.so'):
+def get_module(where: str = "release/lib/hgspy*.so"):
     lib_path = next(glob.iglob(where))
-    loader = importlib.machinery.ExtensionFileLoader('hgspy', lib_path)
+    loader = importlib.machinery.ExtensionFileLoader("hgspy", lib_path)
     spec = importlib.util.spec_from_loader(loader.name, loader)
     hgspy = importlib.util.module_from_spec(spec)
     loader.exec_module(hgspy)
