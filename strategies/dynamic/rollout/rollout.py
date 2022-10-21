881d0cb55d0111c79429cbfdbda170602222a310
l-lan <l.lan@vu.nl>
2022-10-21 22:48:12 +0200

    Remove recursive rollout

--- strategies/dynamic/rollout/rollout.py
+++ strategies/dynamic/rollout/rollout.py
@@ -11,11 +11,10 @@ def rollout(
     obs,
     rng,
     rollout_tlim_factor: float,
-    n_sim_cycles: int,
-    sim_cycle_iters: int,
+    n_simulations: int,
     n_lookahead: int,
     n_requests: int,
-    postpone_threshold: float,
+    dispatch_threshold: float,
     sim_config: dict,
     node_ops: list,
     route_ops: list,
@@ -27,7 +26,7 @@ def rollout(
     those simulations.
     """
     # Return the full epoch instance for first and last epoch
-    if obs["current_epoch"] in [info["start_epoch"], info["end_epoch"]]:
+    if obs["current_epoch"] in [info["end_epoch"]]:
         return obs["epoch_instance"]
 
     # Parameters
@@ -35,43 +34,33 @@ def rollout(
     must_dispatch = set(np.flatnonzero(ep_inst["must_dispatch"]))
     ep_size = ep_inst["is_depot"].size
 
-    # Statistics
-    dispatch_count = np.zeros(ep_size, dtype=int)
-    to_postpone = np.zeros(ep_size, dtype=bool)
-
     rollout_tlim = rollout_tlim_factor * info["epoch_tlim"]
-    sim_tlim = rollout_tlim / n_sim_cycles / sim_cycle_iters
+    sim_tlim = rollout_tlim / n_simulations
 
-    for _ in range(n_sim_cycles):
-        # Simulate ``sim_cycle_iters`` instances and count dispatch actions
-        for _ in range(sim_cycle_iters):
-            sim_inst = simulate_instance(
-                info, obs, rng, n_lookahead, n_requests, to_postpone * 3600
-            )
+    dispatch_count = np.zeros(ep_size, dtype=int)
 
-            res = hgs(
-                sim_inst,
-                hgspy.Config(**sim_config),
-                [getattr(hgspy.operators, op) for op in node_ops],
-                [getattr(hgspy.operators, op) for op in route_ops],
-                [getattr(hgspy.crossover, op) for op in crossover_ops],
-                hgspy.stop.MaxRuntime(sim_tlim),
-            )
+    for _ in range(n_simulations):
+        res = hgs(
+            simulate_instance(info, obs, rng, n_lookahead, n_requests),
+            hgspy.Config(**sim_config),
+            [getattr(hgspy.operators, op) for op in node_ops],
+            [getattr(hgspy.operators, op) for op in route_ops],
+            [getattr(hgspy.crossover, op) for op in crossover_ops],
+            hgspy.stop.MaxRuntime(sim_tlim),
+        )
 
-            best = res.get_best_found()
+        best = res.get_best_found()
 
-            for sim_route in best.get_routes():
-                # Only dispatch routes that contain must dispatch requests
-                if any(idx in must_dispatch for idx in sim_route):
-                    dispatch_count[sim_route] += 1
+        for sim_route in best.get_routes():
+            # Only dispatch routes that contain must dispatch requests
+            if any(idx in must_dispatch for idx in sim_route):
+                dispatch_count[sim_route] += 1
 
-            dispatch_count[0] += 1  # Depot is also "dispatched"
-
-        # Select requests to postpone based on thresholds
-        postpone_count = sim_cycle_iters - dispatch_count
-        to_postpone = postpone_count >= postpone_threshold * sim_cycle_iters
-        dispatch_count *= 0  # reset dispatch count
-
-    dispatch = ep_inst["is_depot"] | ep_inst["must_dispatch"] | ~to_postpone
+    dispatch = (
+        ep_inst["is_depot"]
+        | ep_inst["must_dispatch"]
+        # Only dispatch requests that are dispatched in enough simulations
+        | (dispatch_count >= max(1, n_simulations) * dispatch_threshold)
+    )
 
     return filter_instance(ep_inst, dispatch)
