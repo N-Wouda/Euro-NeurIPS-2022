21724404f5246cac47c30f7621f40b91fd996b3a
Niels Wouda <N-Wouda@users.noreply.github.com>
2022-10-26 22:07:22 +0200

    Static tuning results and new parameter generation script (#147)
    * Simple tuning parameter script
    
    * Write toml files for use with benchmark script
    
    * Remove old tuning script
    
    * Analyse results, benchmark with tuned parameters
    
    * Update penalty defaults
    
    * Update LS defaults

--- hgs_vrptw/include/Config.h
+++ hgs_vrptw/include/Config.h
@@ -14,27 +14,24 @@ struct Config
     int timeLimit = INT_MAX;         // time limit in seconds
     bool collectStatistics = false;  // collect runtime statistics?
 
-    // This was the default until now, but with this value feasible individuals
-    // often become infeasible during the local search in educate. This does not
-    // happen with initialTimeWarpPenalty = 10.
-    size_t initialTimeWarpPenalty = 1;
+    size_t initialTimeWarpPenalty = 6;
 
-    size_t nbPenaltyManagement = 100;  // manage penalties every # iterations
-    double feasBooster = 2.0;          // special increase penalty if no feas
-    double penaltyIncrease = 1.2;      // regular increase if below target feas
-    double penaltyDecrease = 0.85;     // regular decrease if above target feas
+    size_t nbPenaltyManagement = 47;  // manage penalties every # iterations
+    double feasBooster = 2.5;         // special increase penalty if no feas
+    double penaltyIncrease = 1.34;    // regular increase if below target feas
+    double penaltyDecrease = 0.32;    // regular decrease if above target feas
 
     size_t minPopSize = 25;
-    size_t generationSize = 40;   // max size before culling a generation
-    size_t nbElite = 4;           // number of elite individuals in pop
-    double lbDiversity = 0.1;     // minimum pct difference in parent diversity
-    double ubDiversity = 0.5;     // maximum pct difference in parent diversity
-    size_t nbClose = 5;           // # individuals when calculating diversity
-    double targetFeasible = 0.4;  // target feasible pop fraction
-    size_t nbKeepOnRestart = 0;   // # individuals to keep when restarting
+    size_t generationSize = 40;    // max size before culling a generation
+    size_t nbElite = 4;            // number of elite individuals in pop
+    double lbDiversity = 0.1;      // minimum pct difference in parent diversity
+    double ubDiversity = 0.5;      // maximum pct difference in parent diversity
+    size_t nbClose = 5;            // # individuals when calculating diversity
+    double targetFeasible = 0.43;  // target feasible pop fraction
+    size_t nbKeepOnRestart = 0;    // # individuals to keep when restarting
 
-    size_t repairProbability = 50;  // probability of repair if infeasible
-    size_t repairBooster = 10;      // penalty booster when repairing
+    size_t repairProbability = 79;  // probability of repair if infeasible
+    size_t repairBooster = 12;      // penalty booster when repairing
 
     size_t selectProbability = 90;  // offspring selection probability
 
@@ -42,53 +39,52 @@ struct Config
 
     // Granular search parameter, limits the number of moves in the RI local
     // search
-    size_t nbGranular = 40;
+    size_t nbGranular = 34;
 
-    // See Vidal 2012, HGS for VRPTW. Multiplied by 10 for integer arithmetic.
-    int weightWaitTime = 2;   // weight for wait-time nearness
-    int weightTimeWarp = 10;  // weight for time warp nearness
+    int weightWaitTime = 18;  // weight for wait-time nearness
+    int weightTimeWarp = 20;  // weight for time warp nearness
 
     bool shouldIntensify = true;  // try to further improve a new best solution?
 
     // Margin to take (in degrees 0 - 359 as ints [0 - 65536]) to determine
     // overlap of circle sectors for SWAP*
-    int circleSectorOverlapTolerance = 0;
+    int circleSectorOverlapTolerance = static_cast<int>(216 / 360. * 65536);
 
     // Minimum size (in degrees as ints [0 - 65536]) for circle sectors such
     // that even small circle sectors have 'overlap'
-    int minCircleSectorSize = static_cast<int>(15 / 360. * 65536);
+    int minCircleSectorSize = static_cast<int>(332 / 360. * 65536);
 
     // Number of nodes we improve by enumeration in LS postprocessing
-    size_t postProcessPathLength = 6;
+    size_t postProcessPathLength = 7;
 
     explicit Config(int seed = 0,
                     size_t nbIter = 10'000,
                     int timeLimit = INT_MAX,
                     bool collectStatistics = false,
-                    size_t initialTimeWarpPenalty = 1,
-                    size_t nbPenaltyManagement = 100,
-                    double feasBooster = 2.,
-                    double penaltyIncrease = 1.2,
-                    double penaltyDecrease = 0.85,
+                    size_t initialTimeWarpPenalty = 6,
+                    size_t nbPenaltyManagement = 47,
+                    double feasBooster = 2.5,
+                    double penaltyIncrease = 1.34,
+                    double penaltyDecrease = 0.32,
                     size_t minPopSize = 25,
                     size_t generationSize = 40,
                     size_t nbElite = 4,
                     double lbDiversity = 0.1,
                     double ubDiversity = 0.5,
                     size_t nbClose = 5,
-                    double targetFeasible = 0.4,
+                    double targetFeasible = 0.43,
                     size_t nbKeepOnRestart = 0,
-                    size_t repairProbability = 50,
-                    size_t repairBooster = 10,
+                    size_t repairProbability = 79,
+                    size_t repairBooster = 12,
                     size_t selectProbability = 90,
                     int nbVeh = INT_MAX,
-                    size_t nbGranular = 40,
-                    int weightWaitTime = 2,
-                    int weightTimeWarp = 10,
+                    size_t nbGranular = 34,
+                    int weightWaitTime = 18,
+                    int weightTimeWarp = 20,
                     bool shouldIntensify = true,
-                    int circleSectorOverlapToleranceDegrees = 0,
-                    int minCircleSectorSizeDegrees = 15,
-                    size_t postProcessPathLength = 6)
+                    int circleSectorOverlapToleranceDegrees = 216,
+                    int minCircleSectorSizeDegrees = 332,
+                    size_t postProcessPathLength = 7)
         : seed(seed),
           nbIter(nbIter),
           timeLimit(timeLimit),
---
+++ make_static_parameters.py
@@ -0,0 +1,114 @@
+import argparse
+from dataclasses import dataclass
+
+import tomli_w
+from scipy.stats import qmc
+
+
+@dataclass
+class Integer:
+    interval: tuple[int, int]
+    default: int
+
+    def ppf(self, q: float) -> int:
+        lo, hi = self.interval
+        return round(lo + q * (hi - lo))
+
+
+@dataclass
+class Float:
+    interval: tuple[float, float]
+    default: float
+
+    def ppf(self, q: float) -> float:
+        lo, hi = self.interval
+        return lo + q * (hi - lo)
+
+
+# These parameter groups, ranges, and default values have been discussed in
+# https://github.com/N-Wouda/Euro-NeurIPS-2022/issues/33.
+PARAM_SPACE = dict(
+    penalty=dict(  # penalty management parameters
+        initialTimeWarpPenalty=Integer((1, 25), 1),
+        nbPenaltyManagement=Integer((25, 500), 100),
+        feasBooster=Float((1, 10), 2.0),
+        penaltyIncrease=Float((1, 5), 1.2),
+        penaltyDecrease=Float((0.25, 1), 0.85),
+        targetFeasible=Float((0, 1), 0.4),
+        repairProbability=Integer((0, 100), 50),
+        repairBooster=Integer((1, 25), 10),
+    ),
+    population=dict(  # population management parameters
+        minPopSize=Integer((5, 100), 25),
+        generationSize=Integer((1, 100), 40),
+        nbElite=Integer((0, 25), 4),
+        lbDiversity=Float((0, 0.25), 0.1),
+        ubDiversity=Float((0.25, 1), 0.5),
+        nbClose=Integer((1, 25), 5),
+        nbIter=Integer((1_000, 10_000), 10_000),
+    ),
+    ls=dict(  # local search parameters
+        nbGranular=Integer((10, 100), 40),
+        weightWaitTime=Integer((1, 25), 2),
+        weightTimeWarp=Integer((1, 25), 10),
+        circleSectorOverlapToleranceDegrees=Integer((0, 359), 0),
+        minCircleSectorSizeDegrees=Integer((0, 359), 15),
+        postProcessPathLength=Integer((1, 8), 6),
+    ),
+)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("param_space", choices=PARAM_SPACE.keys())
+    parser.add_argument("--num_samples", type=int, default=100)
+    parser.add_argument("--seed", type=int, default=1)
+    parser.add_argument("--out_dir", default="data/tune")
+
+    return parser.parse_args()
+
+
+def write(where: str, params, exp: int):
+    static = dict(
+        node_ops=[
+            "Exchange10",
+            "Exchange11",
+            "Exchange20",
+            "MoveTwoClientsReversed",
+            "Exchange21",
+            "Exchange22",
+            "TwoOpt",
+        ],
+        route_ops=[
+            "RelocateStar",
+            "SwapStar",
+        ],
+        crossover_ops=[
+            "selective_route_exchange",
+        ],
+        params=params,
+    )
+
+    with open(where + f"/{exp}.toml", "wb") as fh:
+        tomli_w.dump(dict(static=static), fh)
+
+
+def main():
+    args = parse_args()
+    space = PARAM_SPACE[args.param_space]
+
+    default = {name: val.default for name, val in space.items()}
+    write(args.out_dir, default, 1)
+
+    sampler = qmc.LatinHypercube(d=len(space), centered=True, seed=args.seed)
+    samples = sampler.random(args.num_samples - 1)
+
+    for exp, sample in enumerate(samples, 2):
+        values = [param.ppf(val) for param, val in zip(space.values(), sample)]
+        scenario = {name: val for name, val in zip(space.keys(), values)}
+        write(args.out_dir, scenario, exp)
+
+
+if __name__ == "__main__":
+    main()
